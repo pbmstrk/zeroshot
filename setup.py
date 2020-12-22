b56d685b772e16104975979f4e7c6a09443e5bf9
pbmstrk <pbmstrk@users.noreply.github.com>
2020-12-22 15:57:21 +0100

    Added requirements file and setup. Plus minor updates

---
+++ setup.py
@@ -0,0 +1,32 @@
+NAME = "zeroshot"
+AUTHOR = "Paul Baumstark"
+VERSION = "0.1.0"
+
+import os
+from setuptools import setup, find_packages
+
+PATH_ROOT = os.path.dirname(__file__)
+
+
+def load_requirements(path_dir=PATH_ROOT, file_name="requirements.txt"):
+    with open(os.path.join(path_dir, file_name), "r") as file:
+        reqs = [ln.strip() for ln in file.readlines()]
+    return reqs
+
+
+def get_extras_require():
+
+    requirements = {
+        "scripts": ["hydra-core"]
+    }
+    return requirements
+
+
+setup(
+    name=NAME,
+    author=AUTHOR,
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=load_requirements(),
+    extras_require=get_extras_require()
+)
--- zeroshot/model/classifier.py
+++ zeroshot/model/classifier.py
@@ -2,6 +2,7 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 from transformers import BertModel
+import torch.nn.functional as F
 
 
 class TextEncoder(nn.Module):
@@ -10,8 +11,8 @@ class TextEncoder(nn.Module):
         super().__init__()
         self.model = BertModel.from_pretrained(model_name)
 
-    def forward(self, x, attention_mask, **kwargs):
-        return self.model(x, attention_mask, **kwargs)
+    def forward(self, input_ids, **kwargs):
+        return self.model(input_ids, **kwargs)
 
 
 class LabelEncoder(nn.Module):
@@ -21,19 +22,73 @@ class LabelEncoder(nn.Module):
         self.model = BertModel.from_pretrained(model_name)
         self.no_grad = no_grad
 
-    def forward(self, x, attention_mask, **kwargs):
+    def forward(self, input_ids, **kwargs):
         if self.no_grad:
             with torch.no_grad():
-                return self.model(x, attention_mask, **kwargs)
-        return self.model(x, attention_mask, **kwargs)
+                return self.model(input_ids, **kwargs)
+        return self.model(input_ids, **kwargs)
+
+
+class Scorer(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, text_encodings, label_encodings):
+        return torch.einsum('ij, kj->ik', text_encodings, label_encodings)
 
 
 class BiEncoderClassifier(pl.LightningModule):
 
-    def __init__(self, text_encoder, label_encodings):
+    def __init__(self, text_encoder, label_encodings, optimizer,
+            scheduler = None):
         super().__init__()
         self.encoder = text_encoder
+        self.scorer = Scorer()
         self.label_encodings = label_encodings
+        self.opt = optimizer
+        if scheduler is not None:
+            self.schedule = scheduler
 
-    def forward(self, x):
-        pass
+    def forward(self, input_ids, **kwargs):
+        text_encodings = self.encoder(input_ids, **kwargs)[1]
+        return self.scorer(text_encodings, self.label_encodings)
+
+    def step(self, batch, batch_idx, prefix=""):
+
+        inputs, targets = batch
+
+        outputs = self(**inputs)
+
+        # compute loss
+        loss = F.cross_entropy(outputs, targets)
+
+        # compute acc
+        _, pred = torch.max(outputs.data, 1)
+        correct = (pred == targets).sum()
+        acc = correct.float() / len(targets)
+
+        self.log(prefix + "loss", loss)
+        self.log(prefix + "acc", acc)
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+
+        return self.step(batch, batch_idx)
+
+    def validation_step(self, batch, batch_idx):
+
+        return self.step(batch, batch_idx, prefix="val_")
+
+    def test_step(self, batch, batch_idx):
+
+        return self.step(batch, batch_idx, prefix="test_")
+
+    def configure_optimizers(self):
+
+        if hasattr(self, "schedule"):
+            return [self.opt], [self.schedule]
+
+        return self.opt
+
