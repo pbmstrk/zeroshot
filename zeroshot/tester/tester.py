62730a02b5dd3c2e9309065acf681c8cc9cd5847
pbmstrk <pbmstrk@users.noreply.github.com>
2021-01-01 21:26:39 +0000

    updates

---
+++ zeroshot/tester/tester.py
@@ -0,0 +1,36 @@
+import torch
+from ..utils import move_args_to_device
+
+
+class Tester:
+
+    def __init__(self, gpu=True):
+        self.gpu = gpu
+        self.device = self.set_device()
+
+    def test_batch(self, batch):
+        inputs, targets = batch
+        outputs = self.forward(**inputs)
+        
+        _, pred = torch.max(outputs.data, 1)
+        batch_correct = (pred.detach().cpu() == targets).sum()
+        self.metrics["acc"] = self.metrics.get("acc", 0) + batch_correct
+
+    def test(self, model, dataloader):
+        
+        self.model = model
+        self.metrics = {}
+
+        for batch in dataloader:
+            self.test_batch(batch)
+
+        return self.metrics
+
+    def set_device(self):
+        return torch.device("cuda") if self.gpu else torch.device("cpu")
+
+    @move_args_to_device
+    def forward(self, input_ids, **kwargs):
+        with torch.no_grad():
+            return self.model(input_ids, **kwargs)
+
--- zeroshot/utils/__init__.py
+++ zeroshot/utils/__init__.py
@@ -1,3 +1,4 @@
 from .download_utils import download_extract
 from .parser import parse_from_txt_file
 from .custom_error import MisconfigurationError
+from .gpu import move_args_to_device
